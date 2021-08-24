f5255051d7d839a67ce3519b11523e149885a5fd
lee abell <lee.abell@finclusionsystems.com>
2021-08-24 15:44:35 +0100

    first commit

--- pages/admin/dashboard.js
+++ pages/admin/dashboard.js
@@ -47,11 +47,7 @@ function Dashboard() {
   return (
     <div>
       <GridContainer>
-        <GridItem xs={12} sm={6} md={3}>
-          <Card>
-		    <Image src={`/images/reactlogo.png`} layout='fill' />
-          </Card>
-        </GridItem>
+		<Image src={`/images/reactlogo.png`} layout='fill' />
         <GridItem xs={12} sm={6} md={3}>
           <Card>
             <CardHeader color="warning" stats icon>
