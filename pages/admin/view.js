5d42bde163ab934851eb5a464e1a1d5c1f2e4e92
lee abell <lee.abell@finclusionsystems.com>
2021-08-25 16:36:21 +0100

    first commit

--- pages/admin/view.js
+++ pages/admin/view.js
@@ -14,7 +14,7 @@ import Card from "components/Card/Card.js";
 import CardHeader from "components/Card/CardHeader.js";
 import CardBody from "components/Card/CardBody.js";
 import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
-import { Modal, ModalBody, ModalFooter } from "reactstrap";
+import { Modal, ModalBody, ModalFooter, FormGroup, Form } from "reactstrap";
 
 const getMuiTheme = () => createMuiTheme({
     overrides: {
