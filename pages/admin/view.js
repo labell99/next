238f5d8db1378479cb8bbbaaeec32a683c64859d
lee abell <lee.abell@finclusionsystems.com>
2021-08-25 13:42:36 +0100

    first commit

--- pages/admin/view.js
+++ pages/admin/view.js
@@ -388,6 +388,36 @@ const options = {
 
   return (
     <GridContainer>
+
+      <Modal toggle={() => setModalOpen(!modalOpen)} isOpen={modalOpen}>
+        <div className=" modal-header">
+          <h5 className=" modal-title" id="exampleModalLabel">
+            Modal title
+          </h5>
+          <button
+            aria-label="Close"
+            className=" close"
+            type="button"
+            onClick={() => setModalOpen(!modalOpen)}
+          >
+            <span aria-hidden={true}>×</span>
+          </button>
+        </div>
+        <ModalBody>...</ModalBody>
+        <ModalFooter>
+          <Button
+            color="secondary"
+            type="button"
+            onClick={() => setModalOpen(!modalOpen)}
+          >
+            Close
+          </Button>
+          <Button color="primary" type="button">
+            Save changes
+          </Button>
+        </ModalFooter>
+      </Modal>
+
       <GridItem xs={12} sm={12} md={12}>
         <Card>
           <CardHeader color="dark">
