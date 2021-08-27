150af4e06a35b2feba03be4b6c05edccbbdb9542
lee abell <lee.abell@finclusionsystems.com>
2021-08-27 15:52:55 +0100

    first commit

--- pages/admin/view.js
+++ pages/admin/view.js
@@ -409,7 +409,7 @@ const options = {
   return (
     <GridContainer>
       <Dialog onClose={() => setModalOpen(!modalOpen)} open={modalOpen} aria-labelledby="form-dialog-title">
-      <GridItem xs={2} >
+      <GridItem xs={12} >
         <DialogTitle id="confirmation-dialog-title">Review IDS Record {modalInfoi}</DialogTitle>
         <InputGroup>
           <InputGroupAddon addonType="prepend">
