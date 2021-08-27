079a385c02c8748deca921e245f28dfeb2095ee3
lee abell <lee.abell@finclusionsystems.com>
2021-08-27 10:17:09 +0100

    first commit

--- pages/admin/view.js
+++ pages/admin/view.js
@@ -21,6 +21,7 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import TextField from '@material-ui/core/TextField';
+import Tabs from '@material-ui/core/Tabs';
 
 const getMuiTheme = () => createMuiTheme({
     overrides: {
