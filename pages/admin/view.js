88c0120aa6ca76a25b41c4c50d717a4d40d68d34
lee abell <lee.abell@finclusionsystems.com>
2021-08-31 13:14:12 +0100

    first commit

--- pages/admin/view.js
+++ pages/admin/view.js
@@ -46,17 +46,7 @@ const useStyles = makeStyles((theme) => ({
     padding: theme.spacing(2),
     margin: 'auto',
     maxWidth: 500,
-  },
-  image: {
-    width: 128,
-    height: 128,
-  },
-  img: {
-    margin: 'auto',
-    display: 'block',
-    maxWidth: '100%',
-    maxHeight: '100%',
-  },
+  }
 }));
 
 const styles = {
