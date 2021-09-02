897d3d805c2cd8c1f20090756c9a4407d53a16e9
lee abell <lee.abell@finclusionsystems.com>
2021-09-02 13:59:43 +0100

    first commit

--- pages/admin/view.js
+++ pages/admin/view.js
@@ -727,7 +727,7 @@ const options = {
                 <Typography className={classes.heading}>Reference</Typography>
               </AccordionSummary>
               <AccordionDetails>
-                <Grid container spacing={4}  direction="row">
+                <Grid container spacing={4}  direction="col">
                   <Grid item xs={4}>
                     <InputGroup>
 			                <InputGroupAddon addonType="prepend">
@@ -761,7 +761,7 @@ const options = {
                     </InputGroup>
                   </Grid>
                 </Grid>
-                <Grid container spacing={4}  direction="row">
+                <Grid container spacing={4}  direction="col">
               <Grid item xs={4}>
                 <InputGroup>
 			      <InputGroupAddon addonType="prepend">
@@ -795,7 +795,7 @@ const options = {
                 </InputGroup>
               </Grid>
             </Grid>
-                <Grid container spacing={4}  direction="row">
+                <Grid container spacing={4}  direction="col">
               <Grid item xs={4}>
                 <InputGroup>
 			      <InputGroupAddon addonType="prepend">
@@ -829,7 +829,7 @@ const options = {
                 </InputGroup>
               </Grid>
             </Grid>
-                <Grid container spacing={4}  direction="row">
+                <Grid container spacing={4}  direction="col">
                   <Grid item xs={4}>
                     <InputGroup>
 			                <InputGroupAddon addonType="prepend">
