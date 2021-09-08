aee1d338ff1dfdcf3250d5cc394dd5f774982a8a
lee abell <lee.abell@finclusionsystems.com>
2021-09-08 16:34:16 +0100

    first commit

--- pages/admin/view.js
+++ pages/admin/view.js
@@ -442,6 +442,28 @@ const options = {
               >
                 <Typography variant="h5" className={classes.heading}>Test</Typography>
               </AccordionSummary>
+              <AccordionDetails>
+                <Grid container direction="row" spacing={2}>
+                <Grid container spacing={2} alignItems="left" justify="left" direction="column">
+                  <Grid item>
+                    <InputGroup>
+	              <InputGroupAddon addonType="prepend">
+	                <Typography>{entry.text}</Typography>
+	              </InputGroupAddon>
+		      <TextField
+		        id="outlined-multiline-flexible"
+		        label="Multiline"
+		        multiline
+		        fullWidth
+		        className={classes.textField}
+		        margin="normal"
+		        variant="outlined"
+                      />
+                    </InputGroup>
+                  </Grid>
+                </Grid>
+                </Grid>
+              </AccordionDetails>
              </Accordion>))}
 
             <Accordion>
