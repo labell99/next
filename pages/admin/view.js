fffacb9cba5a22d82528f6cb9cabf3daf73b8125
lee abell <lee.abell@finclusionsystems.com>
2021-09-08 18:52:21 +0100

    first commit

--- pages/admin/view.js
+++ pages/admin/view.js
@@ -450,6 +450,32 @@ const options = {
               >
                 <Typography variant="h5" className={classes.heading}> {category.menu} </Typography>
               </AccordionSummary>
+
+	                   <AccordionDetails>
+	                     <Grid container direction="row" spacing={2}>
+	                     <Grid container spacing={2} alignItems="left" justify="left" direction="column">
+	                     category.name.map(subcategory => (
+	                       <Grid item>
+	                         <InputGroup>
+	     	              <InputGroupAddon addonType="prepend">
+	     	                <Typography>{subcategory}</Typography>
+	     	              </InputGroupAddon>
+	     		      <TextField
+	     		        id="outlined-multiline-flexible"
+	     		        label="Multiline"
+	     		        multiline
+	     		        fullWidth
+	     		        className={classes.textField}
+	     		        margin="normal"
+	     		        variant="outlined"
+	                           />
+	                         </InputGroup>
+	                       </Grid>
+	                     ))
+	                     </Grid>
+	                     </Grid>
+	                   </AccordionDetails>
+
             </Accordion>
            ))}
 
