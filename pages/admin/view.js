743923ce2bd61d1a16b750e455c9eb47f245b941
lee abell <lee.abell@finclusionsystems.com>
2021-09-09 10:28:13 +0100

    first commit

--- pages/admin/view.js
+++ pages/admin/view.js
@@ -431,7 +431,7 @@ const options = {
 
 
   var categories=[
-    {id:1, name: [{label: "FACTA. Target"},{label: "FACTA. Genome"},{label: "Spike Variants"},{label: "Other Variants"}], menu:"Structure"}
+    {id:1, name: ["FACTA. Target","FACTA. Genome","Spike Variants","Other Variants"], menu:"Structure"}
   ];
 
   return (
@@ -457,7 +457,7 @@ const options = {
 	                       <Grid item>
 	                         <InputGroup>
 	     	              <InputGroupAddon addonType="prepend">
-	     	                <Typography>{subcategory.label}</Typography>
+	     	                <Typography>{subcategory}</Typography>
 	     	              </InputGroupAddon>
 	     		      <TextField
 	     		        id="outlined-multiline-flexible"
