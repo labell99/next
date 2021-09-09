c7e37e34505ab3c68894e52ee9820ddbcb0409a8
lee abell <lee.abell@finclusionsystems.com>
2021-09-09 10:48:54 +0100

    first commit

--- pages/admin/view.js
+++ pages/admin/view.js
@@ -450,13 +450,15 @@ const options = {
                 <Typography variant="h5" className={classes.heading}> {category.menu} </Typography>
               </AccordionSummary>
 
-	                   <AccordionDetails>
-	                     <Grid container direction="row" spacing={2}>
-	                     <Grid container spacing={2} alignItems="left" justify="left" direction="column">
+	          <AccordionDetails>
+	            <Grid container direction="row" spacing={2}>
+	            <Grid container spacing={2} alignItems="left" justify="left" direction="column">
 
-	                     </Grid>
-	                     </Grid>
-	                   </AccordionDetails>
+                  {category.name.map(subcategory => (<Typography>{subcategory}</Typography>))}
+
+	            </Grid>
+	            </Grid>
+	          </AccordionDetails>
 
             </Accordion>
            ))}
