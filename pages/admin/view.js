de697bc6a44d6fe08e3a72d829601776f85e5246
lee abell <lee.abell@finclusionsystems.com>
2021-09-09 10:56:59 +0100

    first commit

--- pages/admin/view.js
+++ pages/admin/view.js
@@ -422,17 +422,8 @@ const options = {
   const res = data[modalInfo];
   console.log("data out2: ",res);
 
-  var entries=[
-    {id:1, name: "FACTA. Target", menu:"Structure"},
-    {id:2, name: "FACTA. Genome", menu:"Structure"},
-    {id:3, name: "Spike Variants", menu:"Structure"},
-    {id:4, name: "Other Variants", menu:"Structure"}
-  ];
-
-
   var categories=[
-    {id:1, name: ["FACTA. Target","FACTA. Genome","Spike Variants","Other Variants"], menu:"Structure"}
-  ];
+    {id:1, name: ["FACTA. Target","FACTA. Genome","Spike Variants","Other Variants"], menu:"Structure"}];
 
   return (
     <GridContainer>
