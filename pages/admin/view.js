842776f9fa4b2d52bba041abec3d6d42a4230446
lee abell <lee.abell@finclusionsystems.com>
2021-09-10 21:07:25 +0100

    first commit

--- pages/admin/view.js
+++ pages/admin/view.js
@@ -424,13 +424,13 @@ const options = {
 };
 
   var categories=[
-    {id:1, name: ["FACTA. Target","FACTA. Genome","Spike Variants","Other Variants"], menu:"Structure"}];
+    {id:1, name: ["FACTA. Target","FACTA. Genome","Spike Variants","Other Variants"], indexes: [2,3,4,6,7,8,5,13,14,15,16,17,18,19,20,21,22,23,24,25,26,21,22], menu:"Structure"}];
 
   var entries=[
     {id:1, name: ["Phenotypes","Publication Source","REFlinks","Users","Notes","Source Note (SIB)","AmpTec_Sourcecode"], menu:"References"}];
 
   var pentries=[
-    {id:1, name: ["Vaccine Name","Vaccine Type","Vaccine Target","Emergence","Short Name","Long Name","Vaccine Application","PANGO Link","BV-BRC Link","Amino Acid","Conserved","Variant Targets","Description","Category","Type","Signal_Peptide_e","NTD_Mutation","RBD_Mutation","S1_S2_Mutation","S2_Mutation","Sequences","Subcategory","IPReference"], menu:"Characterisation"}];
+    {id:1, name: ["Vaccine Name","Vaccine Type","Vaccine Target","Emergence","Short Name","Long Name","Vaccine Application","PANGO Link","BV-BRC Link","Amino Acid","Conserved","Variant Targets","Description","Category","Type","Signal_Peptide_e","NTD_Mutation","RBD_Mutation","S1_S2_Mutation","S2_Mutation","Sequences","Subcategory","IPReference"], indexes: [2,3,4,6,7,8,5,13,14,15,16,17,18,19,20,21,22,23,24,25,26,21,22], menu:"Characterisation"}];
 
   return (
     <GridContainer>
