f7f60cff2caab303dc1d3539992b485199ce78d1
lee abell <lee.abell@finclusionsystems.com>
2021-09-13 09:35:42 +0100

    first commit

--- assets/jss/nextjs-material-dashboard/components/cardIconStyle.js
+++ assets/jss/nextjs-material-dashboard/components/cardIconStyle.js
@@ -15,7 +15,7 @@ const cardIconStyle = {
       borderRadius: "3px",
       backgroundColor: grayColor[0],
       padding: "15px",
-      marginTop: "-20px",
+      marginTop: "15px",
       marginRight: "15px",
       float: "left",
     },
