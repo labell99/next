748ab4fdc5355e8bff4fd8861c3729571ff64d82
lee abell <lee.abell@finclusionsystems.com>
2021-09-13 11:37:23 +0100

    first commit

--- pages/admin/dashboard.js
+++ pages/admin/dashboard.js
@@ -53,7 +53,7 @@ function Dashboard() {
           <Card>
             <CardHeader color="warning" stats icon>
               <CardIcon color="warning">
-                <IconButton style={{fontSize: '10px'}}>
+                <IconButton style={{fontSize: '12px'}}>
                   <StorageIcon />
                   MRNA-UCV-IDS
                 </IconButton>
@@ -65,7 +65,7 @@ function Dashboard() {
           <Card>
             <CardHeader color="success" stats icon>
               <CardIcon color="success">
-                <IconButton style={{fontSize: '10px'}}>
+                <IconButton style={{fontSize: '12px'}}>
                   <StorageIcon />
                   NORVAX-LNPS
                 </IconButton>
