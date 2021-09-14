26cad2097cc762ef8f6f14f6d1bffbf088938f84
lee abell <lee.abell@finclusionsystems.com>
2021-09-14 15:03:33 +0100

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -42,21 +42,9 @@ export default class MyApp extends App {
 
   componentDidMount() {
     let comment = document.createComment(`
-
 =========================================================
 * * NextJS Material Dashboard v1.1.0 based on Material Dashboard React v1.9.0
 =========================================================
-
-* Product Page: https://www.creative-tim.com/product/nextjs-material-dashboard
-* Copyright 2021 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/nextjs-material-dashboard/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
 `);
     document.insertBefore(comment, document.documentElement);
   }
