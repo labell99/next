0d20c34995756512d42cebbcfeea6bc16697ecd6
lee abell <lee.abell@finclusionsystems.com>
2021-09-15 16:37:06 +0100

    first commit

--- components/Context/dataBContext.js
+++ components/Context/dataBContext.js
@@ -1,6 +1,6 @@
 import { createContext, useContext } from 'react'
 
-const export DataBContext = createContext()
+export const DataBContext = createContext()
 
 export default function useDataBContext() {
 	return useContext(DataBContext);
