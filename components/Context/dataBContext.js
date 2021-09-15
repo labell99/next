f9031b7200eb7ae4cfe415d8ed4a6d16485ae414
lee abell <lee.abell@finclusionsystems.com>
2021-09-15 17:16:13 +0100

    first commit

--- components/Context/dataBContext.js
+++ components/Context/dataBContext.js
@@ -2,3 +2,12 @@ import { createContext } from 'react'
 
 export const DataBContext = createContext()
 
+export function MenuProvider(props) {
+	const {value, children} = props
+
+	return (
+	   <DataBContext.Provider value={value}>
+		{children}
+	   </DataBContext.Provider>
+	)
+}
