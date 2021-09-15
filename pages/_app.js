db076bfcd6d99c41fb8fd347240c3c597ebd9a30
lee abell <lee.abell@finclusionsystems.com>
2021-09-15 20:33:47 +0100

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -15,7 +15,6 @@ import ReactDOM from "react-dom";
 import App from "next/app";
 import Head from "next/head";
 import Router from "next/router";
-import {DataBContext} from 'components/Context/dataBContext';
 import {DataBProvider} from 'components/Context/dataBContext';
 import PageChange from "components/PageChange/PageChange.js";
 import "assets/css/nextjs-material-dashboard.css?v=1.1.0";
