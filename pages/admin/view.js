060e05ea228076b08e5831471a433b3269d038c2
lee abell <lee.abell@finclusionsystems.com>
2021-09-16 21:04:28 +0100

    update db tags

--- pages/admin/view.js
+++ pages/admin/view.js
@@ -460,7 +460,7 @@ const options = {
     var categories=[];
   } else if (dbname === "gb-t-bm-rna-ids") {
     var pentries=[
-    {id:1, name: ["Vaccine Name","Vaccine Type","Vaccine Target","Emergence","Short Name","Long Name","Vaccine Application","PANGO Link","FACTA target","Amino Acid","Conserved","Variant Targets","Description","Category","Type","Subcategory","IPReference"], indexes: [2,3,4,6,7,8,5,11,9,12,13,14,15,16,17,18,30,31,32,33,34,21,22], menu:"Characterisation"}];
+    {id:1, name: ["Vaccine Name","Vaccine Type","Vaccine Target","Emergence","Short Name","Long Name","Vaccine Application","PANGO Link","FACTA target","Amino Acid","Conserved","Variant Targets","Description","Category","Type","Subcategory","IPReference","PublicationSource", "DOI", "REFlinks", "FACTAGenome", "Notes", "Users", "AmpTec_Sourcecode", "RNAseqCHECK" ], indexes: [2,3,4,6,7,8,5,11,9,12,13,14,15,16,17,18,19,21,20,22,10,24,23,27,26], menu:"Characterisation"}];
     var entries=[];
     var categories=[];
   };
