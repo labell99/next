08c229cec31a98d1c562344bf56476e735ce1243
lee abell <lee.abell@finclusionsystems.com>
2021-09-16 16:48:50 +0100

    update db tags

--- pages/admin/view.js
+++ pages/admin/view.js
@@ -376,11 +376,10 @@ const options = {
     var pentries=[
     {id:1, name: ["Vaccine Name","Vaccine Type","Vaccine Target","Emergence","Short Name","Long Name","Vaccine Application","PANGO Link","BV-BRC Link","Amino Acid","Conserved","Variant Targets","Description","Category","Type","Signal_Peptide_e","NTD_Mutation","RBD_Mutation","S1_S2_Mutation","S2_Mutation","Sequences","Subcategory","IPReference"], indexes: [2,3,4,6,7,8,5,13,14,15,16,17,18,19,20,29,30,31,32,33,34,21,22], menu:"Characterisation"}];
   } else if (dbname === "norvax-lnps") {
-    var categories=[
-    {id:1, name: ["FACTA. Target","FACTA. Genome","Spike Variants","Other Variants"], indexes: [9,10,11,12], menu:"Structure"}];
-    var entries=[
-    {id:1, name: ["Phenotypes","Publication Source","REFlinks","Users","Notes","Source Note (SIB)","AmpTec_Sourcecode"], indexes: [23,24,25,26,27,28,35], menu:"References"}];
-    var pentries=[];
+    var pentries=[
+    {id:1, name: ["Name","Version","Lipid 1.1","Lipid 1.2","Lipid 1.3","Lipid 1.4","Lipid 2.1","Lipid 2.2","Lipid 2.3","Lipid 2.4","Lipid 3.1","Lipid 3.2","Lipid 3.3","Lipid 3.4","Lipid 4.1","Lipid 4.2","Lipid 4.3","Lipid 4.4","REF Links","AI Analytics Report","Norvax Notes","SINTEF Notes","Users","Preclinical Data and Toxicology"], indexes: [9,10,11,12], menu:"Formulation"}];
+    var entries=[];
+    var categories=[];
   };
 
   return (
