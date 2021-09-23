fa266b2bdac8fac9b36db9f34e14cf55d73d5a2f
lee abell <lee.abell@finclusionsystems.com>
2021-09-23 15:47:11 +0100

    first commit

--- components/Form/index.js
+++ components/Form/index.js
@@ -47,7 +47,7 @@ const OrderForm = () => {
   const dbname = dbcontext.data;
   const [iniValues, setIniValues] = useState([""]);
   const [valSchema, setValSchema] = useState([""]);
-  const [forTit, setForTit] = useState([""]);
+  const [forTit, setForTit] = useState();
   const [forT, setForT] = useState([""]);
 
   useEffect(() => {
