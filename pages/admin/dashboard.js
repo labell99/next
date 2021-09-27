0772a2fd20a61c49afe54d68045aeb27f012482e
lee abell <lee.abell@finclusionsystems.com>
2021-09-27 15:45:45 +0100

    update

--- pages/admin/dashboard.js
+++ pages/admin/dashboard.js
@@ -70,7 +70,7 @@ function Dashboard() {
 
   return (
     <div>
-      <Typography variant="h6"> Dataset {title} Selected.</Typography>
+      <Typography variant="body1"> Dataset {title} Selected.</Typography>
       <GridContainer>
         <GridItem xs={12} sm={6} md={3}>
           <Card >
